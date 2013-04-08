1c40e7079d1f1a120ff32fdb8d782d10434dfc2d
Asaf Weissman <asafweissm@gmail.com>
2013-04-08 21:36:08 +0300

    i

---
+++ compiler.py
@@ -0,0 +1,34 @@
+import os
+
+def append_function(file, name, parameters, body):
+    
+    header = "def " + name + "(" + parameters + "):\n"
+    file.write(header)
+    lines = body.splitlines(True)
+    for line in lines:
+        file.write("\t" + line)
+    file.write("\n")
+
+
+
+code = '''print("first line of code")\nprint("second line of code")\n'''
+filename = 'output/impl.py'
+ops = {"empty": "",          \
+       "insert": "r, t",     \
+       "remove": "r, s",     \
+       "update": "r, s, u",  \
+       "query": "r, s, C"}
+
+# create dir if it doesnt exist
+d = os.path.dirname(filename)
+if not os.path.exists(d):
+    os.makedirs(d)
+    
+file = open(filename, 'a')
+
+for op in ops:
+    append_function(file, op, ops[op], code)
+
+file.close()
+
+compTree = dict()
---
+++ output/impl.py
@@ -0,0 +1,20 @@
+def insert(r, t):
+	print("first line of code")
+	print("second line of code")
+
+def query(r, s, C):
+	print("first line of code")
+	print("second line of code")
+
+def remove(r, s):
+	print("first line of code")
+	print("second line of code")
+
+def empty():
+	print("first line of code")
+	print("second line of code")
+
+def update(r, s, u):
+	print("first line of code")
+	print("second line of code")
+
